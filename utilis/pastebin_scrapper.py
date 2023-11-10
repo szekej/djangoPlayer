8d0257bd9b388042302e41d306cdcaa0bba33721
Szymon <szekej51@gmail.com>
2023-11-10 23:24:33 +0100

    finally version of scrapper

--- utilis/pastebin_scrapper.py
+++ utilis/pastebin_scrapper.py
@@ -1,7 +1,15 @@
+import os
+import profile
+
+import django
 import requests
 from bs4 import BeautifulSoup
 
-# from player.models import Video
+# Ustawienie zmiennej środowiskowej dla Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoProject1.settings")
+django.setup()
+
+from player.models import Video
 
 
 def scrape_pastebin(pastebin_url):
@@ -12,31 +20,37 @@ def scrape_pastebin(pastebin_url):
         episode_divs = soup.find_all('div', style="font: normal normal 1em/1.2em monospace; margin:0; padding:0; "
                                                   "background:none; vertical-align:top;")
 
-        episodes = []
-
+        episode_title = ''
+        episode_number = ''
+        episode_link = ''
         for div in episode_divs:
-            # Pobierz tekst z diva
             text = div.get_text(strip=True)
-
-            # Podziel tekst na linie
             lines = text.split('\n')
-
-            # Sprawdź, czy linie zawierają numer odcinka i link
-            if len(lines[0].split(' ')) == 2 and lines[0].split(' ')[0].lower() != "sezon":
-                episode_number, episode_title = lines[0].split('. ')
-
-                # Dodaj do listy
-                episodes.append({'number': episode_number, 'title': episode_title})
-                print(episodes)
-
-        # Wyświetl wyniki
-        for episode in episodes:
-            print(f"Numer odcinka: {episode['number']}, Tytuł: {episode['title']}")
-
+            for element in lines:
+                if element and "SEZON" not in element:
+                    try:
+                        parts = element.split('. ', 1)
+                        if len(parts) == 2 and parts[0].isdigit():
+                            episode_title = parts[1]
+                            episode_number = parts[0]
+                        if element.startswith('http'):  # Jeśli element to link
+                            episode_link = element
+                    except Exception as e:
+                        print(f"Error: {e}")
             # # Zapisz do bazy danych
-            # video = Video(title=title, video_url=video_url, pastebin_url=pastebin_url)
-            # video.save()
+            if episode_title and episode_number and episode_link:
+                # Sprawdź, czy istnieje już rekord z takim tytułem i numerem odcinka
+                video, created = Video.objects.get_or_create(title=episode_title, defaults={'video_url': episode_link})
+
+                if not created:
+                    # Rekord już istnieje, zaktualizuj jego video_url
+                    video.video_url = episode_link
+                    video.save()
+                    print("Record updated in the database.")
+                else:
+                    print("New record saved to the database.")
+
 
 if __name__ == "__main__":
-    pastebin_url = "https://pastebin.pl/view/0ce85bfa"  # Zastąp 'xxxxxx' właściwym identyfikatorem Pastebin
+    pastebin_url = "https://pastebin.pl/view/0ce85bfa"
     scrape_pastebin(pastebin_url)
