01488d945f9a0e44a1d1105b4d6d00d4f10d40b6
Szymon <szekej51@gmail.com>
2023-11-11 23:08:23 +0100

    first vision of Video model

--- player/models.py
+++ player/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    video_url = models.URLField()
+    description = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.title
