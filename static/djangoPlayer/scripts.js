713c1e128d3443842c50bb3295588dcc570e613f
Szymon <szekej51@gmail.com>
2024-01-03 21:18:05 +0100

    added button - saved episodes var -> let

--- static/djangoPlayer/scripts.js
+++ static/djangoPlayer/scripts.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('randomEpisode').addEventListener('click', function() {
-    var totalEpisodesElement = document.getElementById('totalEpisodes');
-    var stringValue = totalEpisodesElement.value;                           // get whole big string
+    let totalEpisodesElement = document.getElementById('totalEpisodes');
+    let stringValue = totalEpisodesElement.value;                           // get whole big string
 
     stringValue = stringValue.replace(/[\[\] ]/g, '');
-    var availableEpisodes = stringValue.split(',');                         // convert this string to list of episodes
+    let availableEpisodes = stringValue.split(',');                         // convert this string to list of episodes
 
-    var randomIndex = Math.floor(Math.random() * availableEpisodes.length); // get random episode
-    var randomEpisodeNumber = availableEpisodes[randomIndex];
+    let randomIndex = Math.floor(Math.random() * availableEpisodes.length); // get random episode
+    let randomEpisodeNumber = availableEpisodes[randomIndex];
 
     window.location.href = '/player/video/' + randomEpisodeNumber + '/';    // go to page
     });
@@ -30,24 +30,26 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    var addToQueueButtons = document.querySelectorAll('.add-to-queue-btn');
+    let addToQueueButtons = document.querySelectorAll('.add-to-queue-btn');
     addToQueueButtons.forEach(function (button) {
         button.addEventListener('click', function () {
-            var episodePK = this.getAttribute('data-pk');
+            let episodePK = this.getAttribute('data-pk');
 
             // Pobierz CSRF Token z metatagu
-            var csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+            let csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
 
             // Wyślij request AJAX
-            var xhr = new XMLHttpRequest();
+            let xhr = new XMLHttpRequest();
             xhr.open('POST', '/player/add_to_queue/', true);
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
             xhr.setRequestHeader('X-CSRFToken', csrfToken);  // Dodaj ten nagłówek
             xhr.onload = function () {
                 if (xhr.status === 200) {
-                    var response = JSON.parse(xhr.responseText);
+                    let response = JSON.parse(xhr.responseText);
                     if (response.success) {
-                        console.log('PK zapisane');
+                        console.log('episode saved');
+                        // Wymuś przeładowanie strony
+                        location.reload();
                     } else {
                         console.error(response.message);
                     }
@@ -58,68 +60,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     // Pobierz listę odcinków z Local Storage
-//     var episodesQueue = JSON.parse(localStorage.getItem('episodesQueue')) || [];
-//     var listaKolejki = document.getElementById('lista-kolejki');
-//     var hiddenInput = document.getElementById('base-lista-kolejki');
-//     var buttons = document.querySelectorAll('.add-to-list');
-//
-//     function aktualizujWartoscInputa() {
-//         hiddenInput.value = JSON.stringify(episodesQueue);
-//     }
-//
-//     buttons.forEach(function (button) {
-//         button.addEventListener('click', function () {
-//             var episodeId = button.getAttribute('episode-pk');
-//             episodesQueue.push(episodeId);
-//
-//             console.log('Queue of episodes:', episodesQueue);
-//             console.log('Added episode: ' + episodeId);
-//
-//             // Po dodaniu odcinka do kolejki, zapisz zaktualizowaną listę w Local Storage
-//             localStorage.setItem('episodesQueue', JSON.stringify(episodesQueue));
-//
-//             // Wyświetl zaktualizowaną listę na stronie
-//             wyswietlListeKolejki();
-//         });
-//     });
-//
-//     function wyswietlListeKolejki() {
-//         listaKolejki.innerHTML = '';
-//
-//         // Iteruj przez elementy kolejki i dodaj je do listy
-//         episodesQueue.forEach(function (episodeId) {
-//             var listItem = document.createElement('li');
-//             listItem.textContent = 'Odcinek ' + episodeId;
-//             listaKolejki.appendChild(listItem);
-//
-//             // Ustaw timer na 3000 milisekund (3 sekundy), aby usunąć element z listy
-//             setTimeout(function () {
-//                 listaKolejki.removeChild(listItem);
-//             }, 3000);
-//         });
-//     }
-//     aktualizujWartoscInputa()
-// });
+document.addEventListener('DOMContentLoaded', function () {
 
+    let addToQueueButtons = document.querySelectorAll('.add-to-queue-btn');
+    addToQueueButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            button.classList.add('saved');
+        });
+    });
+});
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     var videoPlayer = document.getElementById('player');
-//
-//     var currentEpisodeNumber = window.location.pathname.split('/').slice(-2,-1)[0];
-//     // Przekieruj użytkownika do następnego odcinka
-//
-//     var nextEpisodeNumber = parseInt(currentEpisodeNumber) + 1;
-//     console.log(currentEpisodeNumber, nextEpisodeNumber)
-//     // window.location.href = '{% url "player:video_detail" %}' + nextEpisodeNumber + '/';
-//     // videoPlayer.addEventListener('ended', function () {
-//     //     // Pobierz aktualny numer odcinka z URL
-//     //     var currentEpisodeNumber = window.location.pathname.split('/').pop();
-//     //     console.log(currentEpisodeNumber)
-//     //
-//     //     // Przekieruj użytkownika do następnego odcinka
-//     //     var nextEpisodeNumber = parseInt(currentEpisodeNumber) + 1;
-//     //     window.location.href = '{% url "player:odtwarzaj_odcinek" %}' + nextEpisodeNumber + '/';
-//     // });
-// });
