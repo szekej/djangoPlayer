390c15873efb82226073e51f7055572bed7813be
Szymon <szekej51@gmail.com>
2024-01-06 23:49:57 +0100

    added remove episode from queue view

--- player/views.py
+++ player/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from django.contrib import messages
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
@@ -41,7 +42,7 @@ def add_to_queue(request):
 
 def list_queue_items(request):
     # Pobierz wszystkie obiekty QueueItem powiązane z zalogowanym użytkownikiem
-    queue_items = QueueItem.objects.filter(user=request.user)
+    queue_items = QueueItem.objects.all()
 
     # Pobierz dane odcinków (tytuł i link) dla każdego obiektu QueueItem
     videos_data = [{'title': item.episode.title,
@@ -49,6 +50,19 @@ def list_queue_items(request):
                     'description': item.episode.description} for item in queue_items]
 
     return render(request, 'player/list_queue_items.html', {'videos_data': videos_data})
+
+
+def remove_from_queue(request):
+    if request.method == 'POST':
+        episode_pk = request.POST.get('episode_pk')
+        try:
+            queue_item = QueueItem.objects.get(user=request.user, episode_id=episode_pk)
+            queue_item.delete()
+            messages.success(request, 'Odcinek został usunięty z kolejki.')
+        except QueueItem.DoesNotExist:
+            messages.error(request, 'Odcinek nie znajduje się w kolejce.')
+
+    return redirect(request.META.get('HTTP_REFERER', 'player:video_list'))      # previous page or 'player:video_list'
 
 
 class SearchEpisodesView(View):
@@ -74,6 +88,8 @@ class ListPlayerView(ListView):
     context_object_name = 'episodes'
 
     def get_context_data(self, **kwargs):
+        queued_episodes = QueueItem.objects.all()
+        queued_episodes = [item.episode.title for item in queued_episodes]
         context = super().get_context_data(**kwargs)
         per_page = int(self.request.GET.get('per_page', 50))  # Pobieranie wybranej liczby odcinków
         paginator = Paginator(context['episodes'], per_page)
@@ -81,7 +97,8 @@ class ListPlayerView(ListView):
         page_obj = paginator.get_page(page_number)
         context = {
             'episodes': page_obj,
-            'per_page': per_page
+            'per_page': per_page,
+            'queued_episodes': queued_episodes
         }
         return context
 
